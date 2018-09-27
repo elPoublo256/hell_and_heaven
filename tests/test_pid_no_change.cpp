615e6f2546c12de64ced134f70f7ac649e05202c
elpoublo <elpoublo@gmail.com>
2018-09-27 20:12:44 +0300

    chenge time

--- tests/test_pid_no_change.cpp
+++ tests/test_pid_no_change.cpp
@@ -2,11 +2,13 @@
 #define BOOST_TEST_MODULE test_pid_no_change
 #include <boost/test/unit_test.hpp>
 #include <unistd.h>
+#include <thread>
 BOOST_AUTO_TEST_CASE(test_test)
 {
  auto cur_pid = getpid();
  for(int i = 0; i < 100; i++)
  {
+	 std::this_thread::sleep_for(15s);
 	 BOOST_CHECK_EQUAL(cur_pid, getpid());
  } 
 }
