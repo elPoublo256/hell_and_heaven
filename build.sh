2ff26a22f57aeef751e1807277c5c0c7949a3c3e
elpoublo <elpoublo@gmail.com>
2018-09-29 22:24:01 +0300

    build in builds/

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 #/bin/bash
 
+cd builds/
 cmake .
 make
 rm CMakeCache.txt cmake_install.cmake Makefile
 rm -r CMakeFiles
-#for file in ./*;
-#do
-#	if [[ $file == "CMakeLists.txt" ]];
-#	then
-#     	echo " there $file"
-#	fi
-#done
+cd ../
