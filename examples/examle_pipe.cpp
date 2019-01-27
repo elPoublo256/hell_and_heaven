aaa7921ef22bd4c87856b102a0982976e28a05ec
elPoublo256 <elpoublo@gmail.com>
2019-01-27 21:14:14 +0300

    wrong example pipe

--- examples/examle_pipe.cpp
+++ examples/examle_pipe.cpp
@@ -6,7 +6,7 @@ class MyWritePipe : public hh::WritePipeProcess
     int fake_main(const hh::Exe_arg &arg)
     {
 
-        for(int i = 0; i < 100; i ++)
+        for(int i = 0; i < 5; i ++)
         {
         std::cout << "Write in pipe"<<std::endl;
         *pipe_write << i;
@@ -17,27 +17,59 @@ class MyWritePipe : public hh::WritePipeProcess
 
 class MyReadPipe : public hh::ReadPipeProcess
 {
-
     int fake_main(const hh::Exe_arg &arg)
     {
-        for(;;)
+        for(int i = 0; i < 5; i ++)
         {
         int from_pipe;
         *pipe_read >> from_pipe;
         std::cout<<"get from pipe (" << from_pipe <<")"<<std::endl;
-        return 0;
         }
+        return 0;
     }
 };
 
+void example_c_pipe()
+{
+    int pipefds[2];
+    auto res = pipe(pipefds);
+    if(res == -1)
+    {
+        std::cout<< "ERROR PIPE ERRNO="<<errno;
+        _exit(1);
+    }
+     int fd_r = pipefds[0];
+     int fd_w = pipefds[1];
+     auto pd = fork();
+     if(pd == 0)
+     {
+         close(fd_r);
+         for(int i = 0; i < 10; i++)
+         {
+             auto c = 10 + i;
+             write(fd_w, &c, sizeof(int));
+         }
+         //_exit(0);
+     }
+     pd = fork();
+         if(pd == 0)
+         {
+             close(fd_w);
+             for(int i = 0; i < 10; i++)
+             {
+                 int c;
+                 read(fd_w, &c, sizeof(int));
+                 std::cout << "GET "<<c<<" FROM PIPE"<<std::endl;
+             }
+             //_exit(0);
+         }
+
+}
+
 int main()
 {
-    MyWritePipe pw;
-    MyReadPipe pr;
-    hh::connect(pr, pw);
-
-    pw.start();
-    pr.start();
-
+  example_c_pipe();
+  //MyWritePipe pw; MyReadPipe pr; hh::connect(pr, pw);
+  //pw.start(); pr.start();
 	return 0;
 }
