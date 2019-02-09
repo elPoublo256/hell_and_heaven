6a3e9b70c28f2826246abddd1bdc586293010340
elPoublo256 <elpoublo@gmail.com>
2019-02-09 18:40:45 +0300

    restart develop

--- core_files/psx_files.cpp
+++ core_files/psx_files.cpp
@@ -1,8 +1,30 @@
 #include "psx_files.h"
+#include <unistd.h>
+hh::PSX_File::PSX_File(const std::string &file_name, open_flag_t openflag)
+{
 
+}
+
+
+void hh::PSX_File::lseek_from_begin(const long &nub_bytes)
+{
+
+}
+
+void hh::PSX_File::lseek_from_qurent(const long &nub_bytes)
+{
+
+}
+
+void hh::PSX_File::lseek_from_end(const long &nub_bytes)
+{
+
+}
 
 void hh::copy_psx_file(const PSX_File &original, const PSX_File &copy, std::size_t size_bufer)
 {
+    if(size_bufer != 0)
+    {
     auto bufer = malloc(size_bufer);
 
     while (auto sreaded = read(original._file_descriptor, bufer, size_bufer) > 0)
@@ -13,4 +35,19 @@ void hh::copy_psx_file(const PSX_File &original, const PSX_File &copy, std::size
      }
     }
     free(bufer);
+    return;
+    }
+    else {
+        throw hh::PSX_Fiel_Exc(std::string("Zero size bufer"));
+    }
+}
+
+hh::PSX_Directory::PSX_Directory(const std::string &path) :  PSX_File(path, O_DIRECTORY)
+{
+
+}
+
+void hh::copy_psx_file(const PSX_Directory &origina, const PSX_Directory &copy, std::size_t size_bufer)
+{
+
 }
--- core_files/psx_files.h
+++ core_files/psx_files.h
@@ -7,19 +7,30 @@
 #include <iostream>
 #include <memory>
 #include "exception"
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <set>
 namespace hh {
 
-
+#define open_flag_t decltype(O_RDONLY)
 
 class PSX_File
 {
 public:
 
     PSX_File(){}
-    PSX_File(const int& fd);
-    PSX_File(const std::string& file_name);
-
+    PSX_File(const std::string& file_name, open_flag_t openflag);
+    PSX_File(const PSX_File& copy) = delete;
+    PSX_File(PSX_File && rv_copy);
+    void operator =(const PSX_File& copy) = delete;
+    void lseek_from_begin(const long& nub_bytes);
+    void lseek_from_qurent(const long& nub_bytes);
+    void lseek_from_end(const long& nub_bytes);
+    void read(void* dest, const long nu_bytes);
+    void write(void* dest, const long nu_bytes);
      virtual ~PSX_File(){}
+protected:
+
 
 
 friend void copy_psx_file(const PSX_File& origina, const PSX_File& copy,std::size_t size_bufer);
@@ -27,6 +38,8 @@ friend void copy_psx_file(const PSX_File& origina, const PSX_File& copy,std::siz
 protected:
     void set_filedcripter(const int& fdr);
     int _file_descriptor = -1;
+    open_flag_t _open_flag;
+    long _curent_position;
 
 };
 
@@ -38,4 +51,16 @@ struct PSX_Fiel_Exc : public std::runtime_error
 {
     PSX_Fiel_Exc(std::string wat) : runtime_error(wat){}
 };
+
+class PSX_Directory : public PSX_File
+{
+  public:
+    PSX_Directory(const std::string& path);
+
+private:
+    std::string _path;
+    std::set<std::string> files_names;
+    std::set<std::string> dir_names;
+};
+void copy_psx_file(const PSX_Directory &origina, const PSX_Directory &copy, std::size_t size_bufer = 1024);
 }
